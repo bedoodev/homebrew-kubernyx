2e09d5d6510a08e73f3acfd976969729f8ba0c42
User <user@192.168.1.109>
2026-03-26 01:03:29 +0300

    feat: cask version created

---
+++ Casks/kubernyx.rb
@@ -0,0 +1,11 @@
+cask "kubernyx" do
+  version "0.1.0"
+  sha256 "c49c8be9c6f9acec1681d081ff9a66d7a794ccf872f752e5cefe958d3bab47f8"
+
+  url "https://github.com/bedoodev/kubernyx/releases/download/v0.1.0/kubernyx.app.zip"
+  name "Kubernyx"
+  desc "Simple Kubernetes IDE"
+  homepage "https://github.com/bedoodev/kubernyx"
+
+  app "kubernyx.app"
+end
